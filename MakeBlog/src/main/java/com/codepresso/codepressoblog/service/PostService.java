9b1726c798634f80fcdd050198d217c2f7990c76
Uk-jake <mag0225@naver.com>
2024-03-24 15:39:55 +0900

    feat : Blog 글 목록기능 개발

--- MakeBlog/src/main/java/com/codepresso/codepressoblog/service/PostService.java
+++ MakeBlog/src/main/java/com/codepresso/codepressoblog/service/PostService.java
@@ -1,5 +1,23 @@
 package com.codepresso.codepressoblog.service;
 
-public class PostService {
+import com.codepresso.codepressoblog.mapper.PostMapper;
+import com.codepresso.codepressoblog.vo.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class PostService {
+    Logger logger = LoggerFactory.getLogger(getClass());
+    private PostMapper postMapper;
+
+    public PostService(PostMapper postMapper){
+        this.postMapper = postMapper;
+    }
+
+    public List<Post> getAllPost() {
+        return postMapper.findAll();
+    }
 }
