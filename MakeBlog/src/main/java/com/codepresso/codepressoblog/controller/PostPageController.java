31ff90948255e5e745993479cffea579dc775981
Uk-jake <mag0225@naver.com>
2024-03-25 15:33:19 +0900

    feat : Blog 글 상세보기 페이지 개발

--- MakeBlog/src/main/java/com/codepresso/codepressoblog/controller/PostPageController.java
+++ MakeBlog/src/main/java/com/codepresso/codepressoblog/controller/PostPageController.java
@@ -1,9 +1,28 @@
 package com.codepresso.codepressoblog.controller;
 
 import com.codepresso.codepressoblog.service.PostService;
+import com.codepresso.codepressoblog.vo.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Controller
 public class PostPageController {
+
+    private PostService postService;
+
+    public PostPageController(PostService postService){
+        this.postService = postService;
+    }
+
+    @RequestMapping("/post/{id}")
+    public String getPostDetailPage(Model model, @PathVariable Integer id){
+        Post post = postService.getPostById(id);
+        model.addAttribute("post", post);
+        return "post_detail";
+
+    }
 
 }
--- MakeBlog/src/main/resources/static/js/ajax.js
+++ MakeBlog/src/main/resources/static/js/ajax.js
@@ -1,13 +1,39 @@
-$(function(){
+$(function () {
+    $("#more").click(function () {
+        // console.log("clicked!");
 
+        var next_page = parseInt($(this).attr("current-page")) + 1;
+
+        $.ajax({
+            method: "GET",
+            url: "/post",
+            data: {"page": next_page}
+        })
+            .done(function(response) {
+            for(var post of response) {
+                $("#more-posts").append("<div class=\"post-preview\">" +
+                    "<a href=\"/post/" + post.id + "\">" +
+                    "<h2 class=\"post-title\">" +
+                    post.title +
+                    "</h2>\n" +
+                    "<h3 class=\"post-subtitle\">" +
+                    post.content +
+                    "</h3></a><p class=\"post-meta\">Posted by " +
+                    post.name +
+                    "</p></div><hr class=\"my-4\" />");
+            }
+        });
+        $(this).attr("current-page", next_page);
+
+    });
 
     $(".comment-edit").hide();
 
-    $(".comment-edit-form-button").click(function(){
+    $(".comment-edit-form-button").click(function () {
         $(this).closest(".comment_text").find(".comment-edit").show();
     });
 
-    $(".comment-edit-cancel-button").click(function(){
+    $(".comment-edit-cancel-button").click(function () {
         $(this).closest(".comment_text").find(".comment-edit").hide();
     });
 });
